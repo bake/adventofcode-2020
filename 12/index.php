1c17d9e29ddfb778001e36bd226d0319f0134f17
bake <bake@192k.pw>
2020-12-12 21:50:43 +0100

    Day 13: Add part 1 in PHP

---
+++ 12/index.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AoC\Day12;
+
+use Generator;
+
+class Action
+{
+  public function __construct(
+    public string $direction,
+    public int $steps,
+  ) {}
+}
+
+class Point
+{
+  public function __construct(
+    public int $x,
+    public int $y,
+  ) {}
+
+  public function add(Point $p): Point
+  {
+    return new Point(
+      $this->x + $p->x,
+      $this->y + $p->y,
+    );
+  }
+
+  public function multiply(int $r): Point
+  {
+    return new Point(
+      $this->x * $r,
+      $this->y * $r,
+    );
+  }
+}
+
+/**
+ * @param handle $handle
+ * @return Generator<Action> $actions
+ */
+function input($handle):Generator
+{
+  while ($line = fgets($handle)) {
+    yield new Action($line[0], (int) substr($line, 1));
+  }
+}
+
+/**
+ * @param handle $handle
+ * @return int
+ */
+function part1($handle): int{
+  $pos = new Point(0, 0);
+  $dir = new Point(1, 0);
+  $input = input($handle);
+  foreach ($input as $action) {
+    for($a = 0; $a < $action->steps; $a += 90) {
+      // I mainly just wanted an excure to use match.
+      $dir = match ($action->direction) {
+        'L' => new Point($dir->y, -$dir->x),
+        'R' => new Point(-$dir->y, $dir->x),
+        default => $dir,
+      };
+    }
+    $delta = match ($action->direction) {
+      'N' => new Point(0, -$action->steps),
+      'E' => new Point($action->steps, 0),
+      'S' => new Point(0, $action->steps),
+      'W' => new Point(-$action->steps, 0),
+      'F' => $dir->multiply($action->steps),
+      default => new Point(0,0),
+    };
+    $pos = $pos->add($delta);
+  }
+  return abs($pos->x) + abs($pos->y);
+}
+
+echo part1(STDIN);
