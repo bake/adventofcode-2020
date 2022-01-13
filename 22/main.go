16240e7642ee0ba576708cc389d3c4000dbf7e2b
bake <bake@192k.pw>
2022-01-13 22:16:53 +0100

    Day 22 add part 2

--- 22/main.go
+++ 22/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -21,10 +22,33 @@ func run() error {
 		return err
 	}
 	fmt.Println(part1(p1, p2))
+	fmt.Println(part2(p1, p2))
 	return nil
 }
 
 type player []int
+
+func (p player) score() int {
+	var sum int
+	for i, v := range p {
+		sum += (len(p) - i) * v
+	}
+	return sum
+}
+
+func (p player) encode() string {
+	var sb strings.Builder
+	for _, i := range p {
+		sb.WriteString(strconv.Itoa(i) + ",")
+	}
+	return sb.String()
+}
+
+func (p player) copy() player {
+	q := make(player, len(p))
+	copy(q, p)
+	return q
+}
 
 func input(r io.Reader) (player, player, error) {
 	var ps []player
@@ -57,16 +81,42 @@ func part1(p1, p2 player) int {
 			p1, p2 = p1[1:], append(p2, p1[0])
 		}
 	}
-	var winner []int
-	switch len(p1) > len(p2) {
-	case true:
-		winner = p1
-	case false:
-		winner = p2
+	if len(p1) > len(p2) {
+		return p1.score()
 	}
-	var sum int
-	for i := 0; i < len(winner); i++ {
-		sum += (len(winner) - i) * winner[i]
+	return p2.score()
+}
+
+func recursiveCombat(p1, p2 player) (bool, int) {
+	seen := map[string]interface{}{}
+	for len(p1) > 0 && len(p2) > 0 {
+		key := p1.encode() + "|" + p2.encode()
+		if _, ok := seen[key]; ok {
+			return true, p1.score()
+		}
+		seen[key] = nil
+
+		winner := p1[0] > p2[0]
+		if p1[0] < len(p1) && p2[0] < len(p2) {
+			winner, _ = recursiveCombat(p1.copy()[1:p1[0]+1], p2.copy()[1:p2[0]+1])
+		}
+		switch winner {
+		case true:
+			p1 = append(p1[1:], p1[0])
+			p1, p2 = append(p1, p2[0]), p2[1:]
+		case false:
+			p2 = append(p2[1:], p2[0])
+			p1, p2 = p1[1:], append(p2, p1[0])
+		}
 	}
-	return sum
+
+	if len(p1) > len(p2) {
+		return true, p1.score()
+	}
+	return false, p2.score()
+}
+
+func part2(p1, p2 player) int {
+	_, score := recursiveCombat(p1, p2)
+	return score
 }
